4fff6d0b7fd8c94cfbeb9918fa034e5a51a9d347
Nepster <nepsterxxx@mail.ru>
2020-09-22 11:42:06 +0300

    spike

--- app/common/schema/seo_text.sql
+++ app/common/schema/seo_text.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc035b32dfe3e52fa33a0f748eff98ca5d47777944b5abc6bdd655c42765d80a
-size 24504
+oid sha256:4f07f0c98210d4c3b6e1eee345e587c7fd36786125b952d7b6fbac7d04ce560e
+size 24461
