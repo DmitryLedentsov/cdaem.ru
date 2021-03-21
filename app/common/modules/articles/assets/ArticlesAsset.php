71c8c101748a2428ac76eca7e39ff3098cc78909
andreysemenov <a.semenov@travellizy.com>
2021-03-21 10:30:02 +0200

    articles

---
+++ app/common/modules/articles/assets/ArticlesAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace common\modules\articles\assets;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * Менеджер ресурсов
+ * @package common\modules\articles\assets
+ */
+class ArticlesAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+//    public $sourcePath = '@frontend/themes/basic/assets';
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+
+    public $css = [
+        '/_new/css/pages/static.min.css',
+    ];
+
+}
