19136bc130c6bce6b3c5df00624663f8324f33c4
Nepster <nepsterxxx@mail.ru>
2021-04-06 22:55:52 +0300

    fix news

--- app/assets/src/js/modules/auth.js
+++ app/assets/src/js/modules/auth.js
@@ -5,76 +5,33 @@
     $(document).on('submit', '#form-signIn', function (e) {
         e.preventDefault();
         let $form = $(this);
-        window.ajaxRequest($form, function () {
-            /* return {
-                 beforeSend: function () {},
-                 complete: function () {},
-                 success: function (response) {
-                 }
-             };*/
-        });
+        window.ajaxRequest($form  /*{
+             beforeSend: function () {},
+             complete: function () {},
+             success: function () {}
+         }*/);
     });
 
     // SignUp
     $(document).on('submit', '#form-signup', function (e) {
         e.preventDefault();
         let $form = $(this);
-        window.ajaxRequest($form, function () {
-            /* return {
-                 beforeSend: function () {},
-                 complete: function () {},
-                 success: function (response) {
-                 }
-             };*/
-        });
+        window.ajaxRequest($form  /*{
+             beforeSend: function () {},
+             complete: function () {},
+             success: function () {}
+         }*/);
     });
 
     // Password recovery
     $(document).on('submit', '#form-recovery', function (e) {
         e.preventDefault();
         let $form = $(this);
-        window.ajaxRequest($form, function () {
-            /* return {
-                 beforeSend: function () {},
-                 complete: function () {},
-                 success: function (response) {
-                 }
-             };*/
-        });
+        window.ajaxRequest($form  /*{
+             beforeSend: function () {},
+             complete: function () {},
+             success: function () {}
+         }*/);
     });
-
-
-/*
-    $('#form-signIn').displayValidation({
-        'email': [],
-        'password': ['Неверный пароль'],
-    });
-
-
-    $('#form-forgotPassword').displayValidation({
-        'email': ['Пользователь с таким Email не найден'],
-    });
-
-
-    $('#form-signUp').displayValidation({
-        'email': ['Пользователь с таким Email уже зарегистрирован'],
-        'phone': ['Пользователь с таким телефоном уже зарегистрирован'],
-        'password': ['Вы ввели слишком слабый пароль'],
-        'name': ['Введите ваше имя'],
-        'user_type': ['Указан некорректный тип аренды'],
-    });
-
-    $(document).on('submit', '#form-signUp', function (e) {
-        e.preventDefault();
-
-        $('#form-signUp').displayValidation({
-            'email': ['Пользователь с таким Email уже зарегистрирован'],
-            'phone': ['Пользователь с таким телефоном уже зарегистрирован'],
-            'password': ['Вы ввели слишком слабый пароль'],
-            'name': ['Введите ваше имя'],
-            'user_type': ['Указан некорректный тип аренды'],
-        });
-    });*/
-
 
 })(jQuery);
