15d5b6b150f67b293f7faaabb32e1c1d7e870bb0
nepster <nepsterxxx@mail.ru>
2023-04-14 12:17:17 +0200

    fix modal

--- docker/hosts.sh
+++ docker/hosts.sh
@@ -4,7 +4,8 @@ RED='\033[1;31m'
 NC='\033[0m' # No Color
 
 HOST='cdaem.loc'
-HOST2='control.cdaem.loc'
+HOST2='msk.cdaem.loc'
+HOST3='control.cdaem.loc'
 
 if [ "$EUID" -ne 0 ]
     then echo -e "${RED}Please run with sudo or as root${NC}"
@@ -21,4 +22,10 @@ if grep -q $HOST2 /etc/hosts
     then echo -e "${RED}Host $HOST2 already set${NC}"
 else
    printf "\n127.0.0.1\t\t$HOST2\n" >> /etc/hosts
+fi
+
+if grep -q $HOST3 /etc/hosts
+    then echo -e "${RED}Host $HOST3 already set${NC}"
+else
+   printf "\n127.0.0.1\t\t$HOST3\n" >> /etc/hosts
 fi
