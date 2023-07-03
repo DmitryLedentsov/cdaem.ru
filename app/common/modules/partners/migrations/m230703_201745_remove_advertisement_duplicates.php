8be091e8dd14502c33cbf89c3ee61d01f9ef2645
dimka_228 <ledentd@mail.ru>
2023-07-03 23:27:07 +0300

    made migration

---
+++ app/common/modules/partners/migrations/m230703_201745_remove_advertisement_duplicates.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230703_201745_remove_advertisement_duplicates
+ */
+class m230703_201745_remove_advertisement_duplicates extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        Yii::$app->consoleRunner->run('partners/collector/remove-advertisement-dublicates');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m230703_201745_remove_advertisement_duplicates cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230703_201745_remove_advertisement_duplicates cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
