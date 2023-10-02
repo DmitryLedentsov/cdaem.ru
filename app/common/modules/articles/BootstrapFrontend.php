8b5a8f65fe9e26ec9f3fa44edffffa2b05f88f1d
dimka_228 <ledentd@mail.ru>
2023-10-02 04:01:11 +0300

    rollback wrong cnahges

--- app/common/modules/articles/BootstrapFrontend.php
+++ app/common/modules/articles/BootstrapFrontend.php
@@ -18,6 +18,9 @@ class BootstrapFrontend implements \yii\base\BootstrapInterface
         // Add module URL rules.
         $app->urlManager->addRules(
             [
+                Yii::$app->params['siteSubDomain'] . '/stati' => '/articles/default/index',
+                Yii::$app->params['siteSubDomain'] . '/stati/<id>' => '/articles/default/view',
+
                 'stati' => '/articles/default/index',
                 'stati/<id>' => '/articles/default/view',
             ]
